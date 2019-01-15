b2ecb115f91b544d1523343a31fc0b79459f2170
Aaron Blankstein <aaron@blockstack.com>
2019-01-15 16:24:52 -0600

    back to the CLI

---
+++ src/analyze.js
@@ -0,0 +1,203 @@
+const fetch = require('cross-fetch')
+const config = require('./config.json')
+const fs = require('fs')
+
+const github = 'https://api.github.com'
+
+const org = config.org
+const reposToSkip = config.reposToSkip
+const internals = config.team
+
+
+function isExternal(issue) {
+  return internals.indexOf(issue.user.login) < 0
+}
+
+function median(x) {
+  if (x.length === 0) {
+    return 0
+  }
+  x.sort()
+  if (x.length % 2 == 0) {
+    return (x[Math.floor(x.length/2)] + x[Math.floor(x.length/2)+1])/2
+  } else {
+    return x[Math.floor(x.length/2) + 1]
+  }
+}
+
+function getIssuesStats(issues) {
+  const total = issues.length
+  const closed = issues.filter(issue => issue.state === 'closed').length
+  const daysToFirstResponse = issues.map(issue => {
+    if (issue.commentsData && issue.commentsData.length >= 1) {
+      const issueDate = new Date(issue.created_at)
+      const firstReply = new Date(issue.commentsData[0].created_at)
+      return (firstReply - issueDate) / (1000 * 60 * 60 * 24)
+    } else {
+      return false
+    }
+  })
+  const daysToClose = issues.map(issue => {
+    if (issue.state === 'closed') {
+      const issueDate = new Date(issue.created_at)
+      const firstReply = new Date(issue.closed_at)
+      return (firstReply - issueDate) / (1000 * 60 * 60 * 24)
+    } else {
+      return false
+    }
+  })
+  const issuesWithoutResponse = daysToFirstResponse.filter(x => x === false).length
+  const medianDaysToFirstResponse = median(daysToFirstResponse.filter(x => x !== false))
+  const medianDaysToClose = median(daysToClose.filter(x => x !== false))
+  return {
+    issuesWithoutResponse,
+    issuesOpen: total - closed,
+    issuesClosed: closed,
+    medianDaysToClose: Math.round(10*medianDaysToClose)/10,
+    medianDaysToFirstResponse: Math.round(10*medianDaysToFirstResponse)/10
+  }
+}
+
+function print(stats) {
+  const statRow = [stats.issuesOpen, stats.issuesClosed, stats.issuesWithoutResponse,
+                   stats.medianDaysToClose, stats.medianDaysToFirstResponse]
+
+  return statRow.map(x => `<td>${x}</td>`).join('')
+}
+
+function analyzePRs(repos) {
+  const sinceDate = new Date(Date.now() - MS_IN_A_WEEK)
+  const usersCommented = {}
+
+  const usersOpened = {}
+
+  const allUsers = []
+
+  repos.forEach(repo => {
+    const prs = repo.issues.filter(x => x.pull_request)
+
+    const newPRs = prs.filter(x => {
+      return ((new Date(x.created_at) - sinceDate) >= 0) && !isExternal(x)
+    })
+
+    newPRs.forEach(x => {
+      const pr = x.pull_request.html_url
+      const user = x.user.login
+      if (usersOpened[user]) {
+        usersOpened[user].push(pr)
+      } else {
+        usersOpened[user] = [pr]
+      }
+      if (allUsers.indexOf(user) < 0) {
+        allUsers.push(user)
+      }
+    })
+
+    prs.forEach(x => {
+      const pr = x.pull_request.html_url
+      const recent = x.commentsData.filter( z => (new Date(z.created_at) - sinceDate >= 0))
+      const users = recent.filter(z => !isExternal(z)).map(z => z.user.login)
+      users.forEach(z => {
+        if (usersCommented[z] && usersCommented[z].indexOf(pr) < 0) {
+          usersCommented[z].push(pr)
+        } else {
+          usersCommented[z] = [pr]
+        }
+        if (allUsers.indexOf(z) < 0) {
+          allUsers.push(z)
+        }
+      })
+    })
+  })
+
+  const data = allUsers.map(user => ({user, opened: usersOpened[user], commented: usersCommented[user] }))
+
+  let outputStr = ''
+  outputStr += `<h1>PRs worked on by user</h1>`
+  data.forEach(datum => {
+    outputStr += `<h2>${datum.user}</h2>`
+    const opened = datum.opened ? datum.opened.map(x => `<li>${x}</li>`).join('') : ''
+    const commented = datum.commented ? datum.commented.map(x => `<li>${x}</li>`).join('') : ''
+    outputStr += `<h3>Opened PRs</h3>`
+    outputStr += `<ol>${opened}</ol>`
+    outputStr += `<h3>Commented on PRs</h3>`
+    outputStr += `<ol>${commented}</ol>`
+  })
+
+  return outputStr
+}
+
+function analyzeOSS(repos, fromDate, toDate) {
+  const allIssues = []
+  const externalIssues = []
+  const allPRs = []
+  const externalPRs = []
+
+  const repoAnalysis = repos.map(repo => {
+    const issues = repo.issues.filter(x => {
+      const datetime = new Date(x.created_at)
+      return ((datetime - fromDate >= 0)
+              && (datetime - toDate <= 0))
+    }).filter(x => !(x.pull_request))
+
+    const prs = repo.issues.filter(x => {
+      const datetime = new Date(x.created_at)
+      return ((datetime - fromDate >= 0)
+              && (datetime - toDate <= 0))
+    }).filter(x => x.pull_request)
+
+    const allStats = getIssuesStats(issues)
+    const issuesOpenedByOthers = issues.filter(x => isExternal(x))
+    const externalStats = getIssuesStats(issuesOpenedByOthers)
+
+    const allStatsPRs = getIssuesStats(prs)
+    const PRsOpenedByOthers = prs.filter(x => isExternal(x))
+    const externalStatsPrs = getIssuesStats(PRsOpenedByOthers)
+
+    issues.forEach(x => allIssues.push(x))
+    issuesOpenedByOthers.forEach(x => externalIssues.push(x))
+
+    prs.forEach(x => allPRs.push(x))
+    PRsOpenedByOthers.forEach(x => externalPRs.push(x))
+
+    return { name: repo.name,
+             allStats, externalStats,
+             allStatsPRs, externalStatsPrs }
+  })
+
+  const sumAnalysis = {
+    allStats: getIssuesStats(allIssues),
+    externalStats: getIssuesStats(externalIssues),
+    allPRStats: getIssuesStats(allPRs),
+    externalPRStats: getIssuesStats(externalPRs)
+  }
+
+  const totalIssues = sumAnalysis.externalStats.issuesOpen + sumAnalysis.externalStats.issuesClosed
+  const totalPRs = sumAnalysis.externalPRStats.issuesOpen + sumAnalysis.externalPRStats.issuesClosed
+
+  const allExternal = [].concat(externalIssues).concat(externalPRs)
+
+  const allExternalStats = getIssuesStats(allExternal)
+
+  console.log(`Total issues: ${totalIssues}, Total PRs: ${totalPRs}`)
+  console.log(`Response time: ${allExternalStats.medianDaysToFirstResponse}`)
+}
+
+
+
+async function runAnalysis() {
+  const repoData = JSON.parse(fs.readFileSync('./data.json'))
+  const sinceDate = new Date(process.argv[2]);
+  const untilDate = new Date(process.argv[3]);
+  console.log(`Acquiring data for [${sinceDate}, ${untilDate}]`)
+
+//  localStorage.setItem('repoData', JSON.stringify(repoData))
+//  hideStatusArea()
+
+  const communityMetrics = analyzeOSS(repoData, sinceDate, untilDate)
+//  const prMetrics = analyzePRs(repoData)
+
+
+}
+
+runAnalysis()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fetch = require('cross-fetch')
 const config = require('./config.json')
+const fs = require('fs')
 
 const github = 'https://api.github.com'
 
@@ -7,7 +8,7 @@ const org = config.org
 const reposToSkip = config.reposToSkip
 const internals = config.team
 
-let API_KEY = ''
+let API_KEY = config.apiKey
 let repoData = false
 let count = 0
 
@@ -20,18 +21,6 @@ function authFetch(url) {
 
 function updateStatus() {
   count += 1
-  const counter = document.getElementById(`data-count`)
-  counter.innerHTML = `${count}`
-}
-
-function displayStatusArea() {
-  const statusArea = document.getElementById('status-wrapper')
-  statusArea.classList.remove('invisible')
-}
-
-function hideStatusArea() {
-  const statusArea = document.getElementById('status-wrapper')
-  statusArea.classList.add('invisible')
 }
 
 async function getIssuePageData(repo, startPage, since) {
@@ -86,28 +75,23 @@ function getIssueComments(repo, issue, since) {
   return authFetch(url).then(x => x.json())
 }
 
-async function fetchMonthStatistics() {
-  const sinceDate = new Date(Date.now() - MS_IN_A_MONTH)
+async function fetchStatisticsSince(sinceDate) {
+//  const sinceDate = new Date(Date.now() - MS_IN_A_MONTH)
   const since = sinceDate.toISOString()
-
-  const sinceWeek = new Date(Date.now() - MS_IN_A_WEEK).toISOString()
-
   const repoNames = await getAllRepos()
 
   const repos = await Promise.all(repoNames.map(name => {
     return getIssuePageData(name, 1, since)
       .then(issues => ({ name, issues }))
       .then(repo => {
-        return Promise.all(repo.issues.map(issue => {
-          const createdDate = new Date(issue.created_at)
-          if (createdDate - sinceDate >= 0) {
-            return getIssueComments(repo.name, issue.number)
-              .then(commentsData => Object.assign({}, issue, { commentsData }))
-          } else {
-            return getIssueComments(repo.name, issue.number, sinceWeek)
-              .then(commentsData => Object.assign({}, issue, { commentsData }))
-          }
-        }))
+        return Promise.all(
+          repo.issues
+            .filter(issue => isExternal(issue))
+            .map(issue => {
+              const createdDate = new Date(issue.created_at)
+              return getIssueComments(repo.name, issue.number, sinceDate)
+                .then(commentsData => Object.assign({}, issue, { commentsData }))
+            }))
           .then(issues => ({ name: repo.name, issues }))
       })
   }))
@@ -115,224 +99,15 @@ async function fetchMonthStatistics() {
   return repos
 }
 
-
-function median(x) {
-  if (x.length === 0) {
-    return 0
-  }
-  x.sort()
-  if (x.length % 2 == 0) {
-    return (x[Math.floor(x.length/2)] + x[Math.floor(x.length/2)+1])/2
-  } else {
-    return x[Math.floor(x.length/2) + 1]
-  }
-}
-
-function getIssuesStats(issues) {
-  const total = issues.length
-  const closed = issues.filter(issue => issue.state === 'closed').length
-  const daysToFirstResponse = issues.map(issue => {
-    if (issue.commentsData && issue.commentsData.length >= 1) {
-      const issueDate = new Date(issue.created_at)
-      const firstReply = new Date(issue.commentsData[0].created_at)
-      return (firstReply - issueDate) / (1000 * 60 * 60 * 24)
-    } else {
-      return false
-    }
-  })
-  const daysToClose = issues.map(issue => {
-    if (issue.state === 'closed') {
-      const issueDate = new Date(issue.created_at)
-      const firstReply = new Date(issue.closed_at)
-      return (firstReply - issueDate) / (1000 * 60 * 60 * 24)
-    } else {
-      return false
-    }
-  })
-  const issuesWithoutResponse = daysToFirstResponse.filter(x => x === false).length
-  const medianDaysToFirstResponse = median(daysToFirstResponse.filter(x => x !== false))
-  const medianDaysToClose = median(daysToClose.filter(x => x !== false))
-  return {
-    issuesWithoutResponse,
-    issuesOpen: total - closed,
-    issuesClosed: closed,
-    medianDaysToClose: Math.round(10*medianDaysToClose)/10,
-    medianDaysToFirstResponse: Math.round(10*medianDaysToFirstResponse)/10
-  }
-}
-
 function isExternal(issue) {
   return internals.indexOf(issue.user.login) < 0
 }
 
-function print(stats) {
-  const statRow = [stats.issuesOpen, stats.issuesClosed, stats.issuesWithoutResponse,
-                   stats.medianDaysToClose, stats.medianDaysToFirstResponse]
-
-  return statRow.map(x => `<td>${x}</td>`).join('')
+async function main() {
+  const sinceDate = new Date(process.argv[2]);
+  console.log(`Acquiring data since ${sinceDate}`)
+  const data = await fetchStatisticsSince(sinceDate);
+  fs.writeFileSync("./data.json", JSON.stringify(data, undefined, 2));
 }
 
-function analyzePRs(repos) {
-  const sinceDate = new Date(Date.now() - MS_IN_A_WEEK)
-  const usersCommented = {}
-
-  const usersOpened = {}
-
-  const allUsers = []
-
-  repos.forEach(repo => {
-    const prs = repo.issues.filter(x => x.pull_request)
-
-    const newPRs = prs.filter(x => {
-      return ((new Date(x.created_at) - sinceDate) >= 0) && !isExternal(x)
-    })
-
-    newPRs.forEach(x => {
-      const pr = x.pull_request.html_url
-      const user = x.user.login
-      if (usersOpened[user]) {
-        usersOpened[user].push(pr)
-      } else {
-        usersOpened[user] = [pr]
-      }
-      if (allUsers.indexOf(user) < 0) {
-        allUsers.push(user)
-      }
-    })
-
-    prs.forEach(x => {
-      const pr = x.pull_request.html_url
-      const recent = x.commentsData.filter( z => (new Date(z.created_at) - sinceDate >= 0))
-      const users = recent.filter(z => !isExternal(z)).map(z => z.user.login)
-      users.forEach(z => {
-        if (usersCommented[z] && usersCommented[z].indexOf(pr) < 0) {
-          usersCommented[z].push(pr)
-        } else {
-          usersCommented[z] = [pr]
-        }
-        if (allUsers.indexOf(z) < 0) {
-          allUsers.push(z)
-        }
-      })
-    })
-  })
-
-  const data = allUsers.map(user => ({user, opened: usersOpened[user], commented: usersCommented[user] }))
-
-  let outputStr = ''
-  outputStr += `<h1>PRs worked on by user</h1>`
-  data.forEach(datum => {
-    outputStr += `<h2>${datum.user}</h2>`
-    const opened = datum.opened ? datum.opened.map(x => `<li>${x}</li>`).join('') : ''
-    const commented = datum.commented ? datum.commented.map(x => `<li>${x}</li>`).join('') : ''
-    outputStr += `<h3>Opened PRs</h3>`
-    outputStr += `<ol>${opened}</ol>`
-    outputStr += `<h3>Commented on PRs</h3>`
-    outputStr += `<ol>${commented}</ol>`
-  })
-
-  return outputStr
-}
-
-function analyzeOSS(repos) {
-  const sinceDate = new Date(Date.now() - MS_IN_A_MONTH)
-  const allIssues = []
-  const externalIssues = []
-  const allPRs = []
-  const externalPRs = []
-
-  const repoAnalysis = repos.map(repo => {
-    const issues = repo.issues.filter(x => {
-      return (new Date(x.created_at) - sinceDate >= 0)
-    }).filter(x => !(x.pull_request))
-
-    const prs = repo.issues.filter(x => {
-      return (new Date(x.created_at) - sinceDate >= 0)
-    }).filter(x => x.pull_request)
-
-    const allStats = getIssuesStats(issues)
-    const issuesOpenedByOthers = issues.filter(x => isExternal(x))
-    const externalStats = getIssuesStats(issuesOpenedByOthers)
-
-    const allStatsPRs = getIssuesStats(prs)
-    const PRsOpenedByOthers = prs.filter(x => isExternal(x))
-    const externalStatsPrs = getIssuesStats(PRsOpenedByOthers)
-
-    issues.forEach(x => allIssues.push(x))
-    issuesOpenedByOthers.forEach(x => externalIssues.push(x))
-
-    prs.forEach(x => allPRs.push(x))
-    PRsOpenedByOthers.forEach(x => externalPRs.push(x))
-
-    return { name: repo.name,
-             allStats, externalStats,
-             allStatsPRs, externalStatsPrs }
-  })
-
-  const sumAnalysis = {
-    allStats: getIssuesStats(allIssues),
-    externalStats: getIssuesStats(externalIssues),
-    allPRStats: getIssuesStats(allPRs),
-    externalPRStats: getIssuesStats(externalPRs)
-  }
-
-  let outputStr = ''
-
-  outputStr += `<h3>Statistics for issues/PRs created since ${sinceDate}</h3>`
-  outputStr += '<br/><table class="table">'
-  outputStr += '<tr><td></td><td colspan=5>issues</td><td colspan=5>PRs</td></tr>'
-  outputStr += '<tr><td></td><td>open</td><td>closed</td><td>no response</td><td>days to close</td><td>days to response</td><td>open</td><td>closed</td><td>no response</td><td>days to close</td><td>days to response</td></tr>'
-  outputStr += `<tr><td>All creators</td>${print(sumAnalysis.allStats)}${print(sumAnalysis.allPRStats)}</tr>`
-  outputStr += `<tr><td>External only</td>${print(sumAnalysis.externalStats)}${print(sumAnalysis.externalPRStats)}</tr>`
-  outputStr += '</table><br/>'
-
-  outputStr += '<h3>Per Repo Statistics</h3>'
-  outputStr += '<br/><table class="table">'
-  outputStr += '<tr><td></td><td colspan=5>issues</td><td colspan=5>PRs</td></tr>'
-  outputStr += '<tr><td></td><td>open</td><td>closed</td><td>no response</td><td>days to close</td><td>days to response</td><td>open</td><td>closed</td><td>no response</td><td>days to close</td><td>days to response</td></tr>'
-
-  repoAnalysis.forEach(repo => {
-    if (repo.allStats.issuesOpen + repo.allStats.issuesClosed > 0) {
-      outputStr += `<tr><td>${repo.name.padStart(20).slice(0, 20)}</td>${print(repo.allStats)}${print(repo.allStatsPRs)}</tr>`
-      outputStr += `<tr><td>External only</td>${print(repo.externalStats)}${print(repo.externalStatsPrs)}</tr>`
-    }
-  })
-
-  outputStr += '</table>'
-  return outputStr
-}
-
-
-async function runAnalysis(forceFetch) {
-  const apiKey = document.getElementById('api-key').value.trim()
-  API_KEY = apiKey
-
-  if (!forceFetch && localStorage.hasOwnProperty('repoData')) {
-    repoData = JSON.parse(localStorage.getItem('repoData'))
-  } else {
-    displayStatusArea()
-    repoData = await fetchMonthStatistics()
-  }
-
-
-  localStorage.setItem('repoData', JSON.stringify(repoData))
-//  hideStatusArea()
-
-  const communityMetrics = analyzeOSS(repoData)
-  const prMetrics = analyzePRs(repoData)
-
-  const messageContainer = document.getElementById('message-wrapper')
-  const messageArea = document.getElementById('message')
-
-  messageContainer.classList.remove('invisible')
-  messageArea.innerHTML = communityMetrics
-
-  const messageContainerPR = document.getElementById('pr-message-wrapper')
-  const messageAreaPR = document.getElementById('pr-message')
-
-  messageContainerPR.classList.remove('invisible')
-  messageAreaPR.innerHTML = prMetrics
-
-}
-
-window.runAnalysis = runAnalysis
+main()
